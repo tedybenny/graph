709be3f43f4120ba73bad9cd7ab1d4c415566f37
Горшков Иван <disxack@gmail.com>
2020-10-29 16:35:25 +0300

    Добавьте файлы проекта.

---
+++ DaCheTakSlozhno/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using OxyPlot;
+using OxyPlot.Series;
+using System.Data;
+
+namespace DaCheTakSlozhno
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Model XUY = new Model();
+        public string Xmin
+        {
+            get => XUY.Xmin;
+            set => XUY.Xmin = value;
+        }
+        public string Xmax
+        {
+            get => XUY.Xmax;
+            set => XUY.Xmax = value;
+        }
+
+        public MainWindow()
+        {
+            this.Title = "Example 2";
+            this.Points = new List<DataPoint>
+                              {
+                                  new DataPoint(0, 4),
+                                  new DataPoint(10, 13),
+                                  new DataPoint(20, 15),
+                                  new DataPoint(30, 16),
+                                  new DataPoint(40, 12),
+                                  new DataPoint(50, 12)
+                              };
+            DataContext = this;
+        }
+
+
+        public IList<DataPoint> Points { get; private set; }
+    }
+    
+
+
+        
+}
---
+++ DaCheTakSlozhno/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaCheTakSlozhno
+{
+    class Model
+    {
+        List <Point> points = new List<Point>();
+        public string Xmin
+        {
+            get;
+            set;
+        }
+        public string Xmax
+        {
+            get;
+            set;
+        }
+
+    }
+}
